cb969d616ca51e12c9980b85257d0b7a1b116a61
Chris <chrischeung1126@outlook.com>
2021-10-31 17:33:52 +0800

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+  return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  new_data = request.get_json()
+  print(new_data)
+  incomes.append(new_data )
+  print(incomes)
+  return jsonify(incomes), 204
 
 if __name__ == '__main__':
   app.run(host='127.0.0.4', port=80, debug=True)
